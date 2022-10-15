2154e3346352543d15dafc50dff0db49e0de3b78
Bill <jzhan411@ucsc.deu>
2022-10-14 22:42:21 -0700

    Very basic version of CSV Node with no styling

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CSVNode from "../Nodes/CSVNode";
 
 export default function Sidebar() {
     const onDragStart = (event, nodeType) => {
@@ -8,28 +9,17 @@ export default function Sidebar() {
 
     return (
         <aside>
-            <div className="description">
-                You can drag the nodes between the pane.
+            <div className="description">You can drag the nodes between the pane.</div>
+            <div className="dndnode CSVNode" onDragStart={(event) => onDragStart(event, "CSVNode")} draggable>
+                CSV Input Node
             </div>
-            <div
-                className="dndnode input"
-                onDragStart={(event) => onDragStart(event, "input")}
-                draggable
-            >
+            <div className="dndnode input" onDragStart={(event) => onDragStart(event, "input")} draggable>
                 Input Node
             </div>
-            <div
-                className="dndnode output"
-                onDragStart={(event) => onDragStart(event, "output")}
-                draggable
-            >
+            <div className="dndnode output" onDragStart={(event) => onDragStart(event, "output")} draggable>
                 Output Node
             </div>
-            <div
-                className="dndnode default"
-                onDragStart={(event) => onDragStart(event, "default")}
-                draggable
-            >
+            <div className="dndnode default" onDragStart={(event) => onDragStart(event, "default")} draggable>
                 Default Node
             </div>
         </aside>
