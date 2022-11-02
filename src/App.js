072a246d93029733f6c518304e701822a6eb5421
Bill Zhang <jzhan411@ucsc.edu>
2022-11-02 13:00:47 -0700

    Test?

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import { CssBaseline, Stack } from "@mui/material";
 import "./App.css";
 import Canvas from "./Canvas";
 import Sidebar from "./Components/Sidebar";
-import { SocketProvider } from "./Nodes/SocketProvider";
 
 function App() {
     return (
         <>
-            <SocketProvider>
-                <CssBaseline />
-                <Stack direction="row">
-                    <Canvas />
-                    <Sidebar />
-                </Stack>
-            </SocketProvider>
+            <CssBaseline />
+            <Stack direction="row">
+                <Canvas />
+                <Sidebar />
+            </Stack>
         </>
     );
 }
