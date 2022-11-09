a2886c4918fd5816e0cded3a6bfb7b2a72c0a906
Jingtian Zhang <jzhan411@ucsc.edu>
2022-11-08 22:10:59 -0800

    Add hash routing to the app
    Includes a landing page with a sign-up button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { CssBaseline, Stack } from "@mui/material";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import "./App.css";
 import Canvas from "./Canvas";
 import Sidebar from "./Components/Sidebar";
+import LandingPage from "./LandingPage";
 
 function App() {
     return (
         <>
-            <CssBaseline />
-            <Stack direction="row">
-                <Canvas />
-                <Sidebar />
-            </Stack>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route
+                        path="/trialCanvas"
+                        element={
+                            <>
+                                <CssBaseline />
+                                <Stack direction="row">
+                                    <Canvas />
+                                    <Sidebar />
+                                </Stack>
+                            </>
+                        }
+                    />
+                </Routes>
+            </HashRouter>
         </>
     );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,48 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const url =
+    "https://images.unsplash.com/photo-1634117622592-114e3024ff27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80";
+
+export default function Hero() {
+    const nav = useNavigate();
+    return (
+        <>
+            <Box
+                width="100vw"
+                maxWidth="100%"
+                height="85vh"
+                sx={{
+                    backgroundImage: `url(${url})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Stack spacing={4} direction="column" alignItems="center">
+                    <Typography variant="h2">Make your data come to life!</Typography>
+                    <Typography variant="h5">Live or static data</Typography>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => nav("/trialCanvas")}
+                        sx={{
+                            backgroundColor: "white",
+                            color: "black",
+
+                            "&:hover": {
+                                backgroundColor: "black",
+                                color: "white",
+                            },
+                        }}
+                    >
+                        Try it out!
+                    </Button>
+                </Stack>
+            </Box>
+        </>
+    );
+}
