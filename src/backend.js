dc8e95eefb9334a764e38def5b0b6d2a1cc278da
Bill Zhang <billzhangsc@gmail.com>
2022-11-16 16:50:56 -0800

    Final version

--- src/backend.js
+++ src/backend.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { NodeIdContext } from "./Nodes/nodes";
 import { useCallback, useContext } from "react";
 
-export const backendUrl = "http://localhost:5000/";
+export const backendUrl = "http://127.0.0.1:5000";
 export const socket = io(backendUrl);
 
 export function useSocketIoChannel(channelName) {
