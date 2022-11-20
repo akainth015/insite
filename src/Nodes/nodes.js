1ed2085cf1b5fbebbf8a39ac2e22e435f1f87f90
Abel-A <aasrese@ucsc.edu>
2022-11-19 22:57:45 -0800

    completed pie chart

--- src/Nodes/nodes.js
+++ src/Nodes/nodes.js
@@ -16,6 +16,8 @@ import FilterNode from "./Modification/Filter";
 import JsonNode from "./Input/JsonNode";
 import Normalization from "./Modification/Normalization";
 import { ButtonNode } from "./Input/ButtonNode";
+import Pie_Chart from "./Output/PieChart";
+import Stock_Ticker from "./Output/StockTicker"; 
 
 export const nodeIcons = {
     "Web Hook": WebhookIcon,
@@ -43,6 +45,7 @@ export const outputNodeTypes = {
     Clock: ClockNode,
     "Text Display": TextNode,
     Correlation: Correlation,
+    "Pie Chart": Pie_Chart,
 };
 
 const nodeStates = {};
@@ -105,6 +108,8 @@ export function useInput(label, inputTypes) {
     return [input, <Handle type={"target"} id={label} />];
 }
 
+
+//
 // The following code allows the Node ID to be implicitly captured by our hook above
 // without needing to be explicitly threaded through the various hooks and functions
 // that get called
@@ -130,3 +135,4 @@ for (const nodeType in inputNodeTypes) {
 for (const nodeType in outputNodeTypes) {
     outputNodeTypes[nodeType] = NodeWrapper(outputNodeTypes[nodeType]);
 }
+
