74f869c9ed597197785e5507672b627e18cae63a
Abel-A <aasrese@ucsc.edu>
2022-11-22 17:15:31 -0800

    progressing stock Node

---
+++ src/Nodes/Output/StockTicker.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import {Card, CardContent} from "@mui/material";
+import {useEffect, useState} from "react";
+import { useOutput, useInput } from "../nodes";
+import { Box, Typography} from "@mui/material";
+import { useSocketIoChannel } from "../../backend";
+import { FilePresent } from "@mui/icons-material";
+
+
+export default function Stock_Ticker(){
+    const [companyName, setCompanyName] = useState("Input desired Company Ticker");
+    const [price, setStockPriceOutput] = useState(0);
+    const [emitStockPriceSubscription, registerToUpdates] = useSocketIoChannel("get_market_price");
+
+    
+
+    const intervalID = null
+    const handleSubmit = () => {
+        clearInterval(intervalID);
+        intervalID = setInterval(emitStockPriceSubscription(companyName), 1000);
+    }
+
+
+    useEffect(() => {
+        return registerToUpdates((data) => {
+            setStockPriceOutput(data);
+        });
+    }, [registerToUpdates]);
+
+
+    return(
+        <Box
+        display="flex"
+        flexDirection="column"
+        sx={{
+            backgroundColor: "white",
+            width: 300,
+            height: 90,
+            padding: 2,
+            borderRadius: 2,
+        }}
+        >
+        <form onSubmit={handleSubmit}>
+        <label>
+            Company Ticker:
+            <input
+            type="text"
+            value= {companyName}
+            onChange={e => setCompanyName(e.target.value)}
+            />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+
+        {price}
+        
+        </Box>
+        
+        
+    )
+
+}
+
